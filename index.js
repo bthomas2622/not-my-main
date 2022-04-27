12ec89a299f1e97a0a430f3aecf00fdd768a269e
bthomas2622 <bthomas2622@gmail.com>
2022-04-26 18:40:40 -0700

    nmm command

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const Discord = require('discord.js');
+const { Client, Intents } = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
+  
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
 
-client.on('message', msg => {
-    if (msg.content === '/nmm') {
-      msg.reply('Pong!');
-    }
-  });
+  const apexLegendList = ['Ash', 'Bangalore', 'Bloodhound', "Caustic", "Crypto", "Fuse", "Gibraltar", "Horizon", "Lifeline", "Loba", "Mad Maggie", "Mirage", "Octane", "Pathfinder", "Rampart", "Revenant", "Seer", "Valkyrie", "Wattson", "Wraith"]
 
-client.login(process.env.DISCORD_TOKEN);
+  if (interaction.commandName === 'nmm') {
+    await interaction.reply(apexLegendList[Math.floor(Math.random() * apexLegendList.length)]);
+  }
+});
+
+client.login("process.env.DISCORD_TOKEN");
