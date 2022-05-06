5171605009ad6f01516e5cba74a96f7baf78bb8c
bthomas2622 <bthomas2622@gmail.com>
2022-05-05 17:34:07 -0700

    new voicelines and commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,12 @@ const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
 const commands = [
+	new SlashCommandBuilder().setName('commands').setDescription("Awoken Bot command list"),
 	new SlashCommandBuilder().setName('nmm').setDescription('Generate a random Apex Legends champion, if you lose it\'s ok! It\'s not your main!'),
+	new SlashCommandBuilder().setName('coinflip').setDescription("Flip a coin"),
+	new SlashCommandBuilder().setName('d6').setDescription("Roll a d6"),
+	new SlashCommandBuilder().setName('d20').setDescription("Roll a d20"),
+	new SlashCommandBuilder().setName('dad').setDescription("..."),
 ]
 	.map(command => command.toJSON());
 
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import legends from './apex_legends.json' assert { type: "json" };
 const { Client, Intents } = require('discord.js');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
@@ -9,11 +10,34 @@ client.on('ready', () => {
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
-  const apexLegendList = ['Ash', 'Bangalore', 'Bloodhound', "Caustic", "Crypto", "Fuse", "Gibraltar", "Horizon", "Lifeline", "Loba", "Mad Maggie", "Mirage", "Octane", "Pathfinder", "Rampart", "Revenant", "Seer", "Valkyrie", "Wattson", "Wraith"]
+  const apexLegendList = Object.keys(legends);
+  // const apexLegendList = ['Ash', 'Bangalore', 'Bloodhound', "Caustic", "Crypto", "Fuse", "Gibraltar", "Horizon", "Lifeline", "Loba", "Mad Maggie", "Mirage", "Octane", "Pathfinder", "Rampart", "Revenant", "Seer", "Valkyrie", "Wattson", "Wraith"]
 
-  if (interaction.commandName === 'nmm') {
-    await interaction.reply(apexLegendList[Math.floor(Math.random() * apexLegendList.length)]);
+  if (interaction.commandName == 'commands') {
+    await interaction.reply("/nmm" + "\n" + "/coinflip" + "\n" + "/d6" + "\n" + "/d20" + "\n" + "/dad");
   }
+  else if (interaction.commandName === 'nmm') {
+    let chosenLegend = apexLegendList[Math.floor(Math.random() * apexLegendList.length)];
+    let chosenLegendVoiceLines = legends[chosenLegend];
+    await interaction.reply(chosenLegend + "\n" + '"' + chosenLegendVoiceLines[Math.floor(Math.random() * chosenLegendVoiceLines.length)] + '"');
+  }
+  else if (interaction.commandName === 'coinflip') {
+    let num = Math.random();
+    let coin;
+    if (num < 0.5) { coin = "HEADS";} else { coin = "TAILS";}
+    await interaction.reply(coin)
+  }
+  else if (interaction.commandName === 'd6') {
+    await interaction.reply(Math.floor(Math.random() * 6) + 1)
+  }
+  else if (interaction.commandName === 'd20') {
+    await interaction.reply(Math.floor(Math.random() * 20) + 1)
+  }
+  else if (interaction.commandName === 'dad') {
+    await interaction.reply("son?")
+  }
+  
+
 });
 
 client.login(process.env.DISCORD_TOKEN);
