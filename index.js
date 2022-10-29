fc0ced5c653e347f9045cca51b3c353f787c99a5
bthomas2622 <bthomas2622@gmail.com>
2022-10-29 12:24:01 -0700

    fix deprecated client login

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const legends = require('./apex_legends.json');
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
