86adee2b59b6d6f6bef1d06bd670ae22f8e3ab66
bthomas2622 <bthomas2622@gmail.com>
2022-11-02 15:11:54 -0700

    trying to get pics sent

--- commands/nmmApex.js
+++ commands/nmmApex.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, AttachmentBuilder } = require('discord.js');
 const legends = require('../apex_legends.json');
 
 module.exports = {
@@ -6,10 +6,14 @@ module.exports = {
 		.setName('apex-not-my-main')
 		.setDescription('Generate a random Apex Legends champion, if you lose it\'s ok! It\'s not your main!'),
 	async execute(interaction) {
-        const apexLegendList = Object.keys(legends);
-        let chosenLegend = apexLegendList[Math.floor(Math.random() * apexLegendList.length)];
-        let chosenLegendVoiceLines = legends[chosenLegend];
-        let response = "**" + chosenLegend + "**" + " - " + '"' + chosenLegendVoiceLines[Math.floor(Math.random() * chosenLegendVoiceLines.length)] + '"';
-        await interaction.reply(response, { files: ['./pics/apex/' + chosenLegend + '.png'] });
+                const apexLegendList = Object.keys(legends);
+                let chosenLegend = apexLegendList[Math.floor(Math.random() * apexLegendList.length)];
+                let chosenLegendVoiceLines = legends[chosenLegend];
+                let response = "**" + chosenLegend + "**" + " - " + '"' + chosenLegendVoiceLines[Math.floor(Math.random() * chosenLegendVoiceLines.length)] + '"';
+                let attachment = new AttachmentBuilder().setName(chosenLegend + '.png').setFile('./pics/apex/' + chosenLegend + '.png');
+                await interaction.reply({
+                        content: response, 
+                        files: [attachment]
+                });
 	},
 };
