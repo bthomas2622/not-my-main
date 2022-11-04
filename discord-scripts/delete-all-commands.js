33229739e8a7eb91db18a003d6dedd6ab089781a
bthomas2622 <bthomas2622@gmail.com>
2022-11-04 10:37:53 -0700

    forgot one filepath

--- discord-scripts/delete-all-commands.js
+++ discord-scripts/delete-all-commands.js
@@ -3,14 +3,9 @@ const { REST, Routes } = require('discord.js');
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
 // for guild-based commands
-rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
-
-// for guild-based commands
-rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_DEV_GUILD_ID), { body: [] })
-	.then(() => console.log('Successfully deleted all guild commands.'))
-	.catch(console.error);
+// rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.DISCORD_GUILD_ID), { body: [] })
+// 	.then(() => console.log('Successfully deleted all guild commands.'))
+// 	.catch(console.error);
 
 // for global commands
 rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), { body: [] })
