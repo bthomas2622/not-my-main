dfe15cb6c3dce58baa9e01976c185f17ba889bcf
bthomas2622 <bthomas2622@gmail.com>
2022-11-23 17:00:51 -0800

    testing free games message

--- bot-scripts/epic-free-games.js
+++ bot-scripts/epic-free-games.js
@@ -4,6 +4,17 @@ const getFreeEPICGamesFormatted = async () => {
     const message = await getGames("US", true).then((games) => {
         const curentFreeGames = games.currentGames.map((game) => `[${game.title}](https://store.epicgames.com/en-US/p/${game.productSlug})`).join("\n");
         const nextFreeGames = games.nextGames.map((game) => `[${game.title}](https://store.epicgames.com/en-US/p/${game.productSlug})`).join("\n");
+        
+        console.log(games.currentGames);
+        // loop through games.currentGames
+        games.currentGames.forEach((game) => {
+            console.log(game.title);
+            console.log(game.urlSlug);
+            console.log(game.url);
+            console.log(game.productSlug);
+            console.log(game.expiryDate);
+        });
+
         return `**EPIC Free Games**\n**Now**\n${curentFreeGames}\n**Coming Soon**\n${nextFreeGames}`;
         }).catch((err) => {
         console.log(err);
