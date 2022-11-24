000ee835a1a454230e95e6629117446d9ffeead6
bthomas2622 <bthomas2622@gmail.com>
2022-11-23 16:41:28 -0800

    testing free games message

--- bot-scripts/epic-free-games.js
+++ bot-scripts/epic-free-games.js
@@ -1,17 +1,15 @@
 const { getGames } = require("epic-free-games");
 
 const getFreeEPICGamesFormatted = async () => {
-    let message = "";
-    await getGames("US", true).then((games) => {
+    const message = await getGames("US", true).then((games) => {
         const curentFreeGames = games.currentGames.map((game) => `[${game.title}](https://store.epicgames.com/en-US/p/${game.productSlug})`).join("\n");
         const nextFreeGames = games.nextGames.map((game) => `[${game.title}](https://store.epicgames.com/en-US/p/${game.productSlug})`).join("\n");
-        message = `**EPIC Free Games**\n**Now**\n${curentFreeGames}\n**Coming Soon**\n${nextFreeGames}`;
-        return message;
+        return `**EPIC Free Games**\n**Now**\n${curentFreeGames}\n**Coming Soon**\n${nextFreeGames}`;
         }).catch((err) => {
-        message = 'Error fetching games.';
         console.log(err);
-        return message;
+        return 'Error fetching games.';
     });
+    return message;
 };
 
 module.exports = { getFreeEPICGamesFormatted };
