de94259631a599818fa9d5e876ae8b49e55653ef
bthomas2622 <bthomas2622@gmail.com>
2022-12-10 15:01:27 -0800

    fix tft not returning anything

--- bot-utils/rankingsFormatter.js
+++ bot-utils/rankingsFormatter.js
@@ -1,6 +1,6 @@
 const rankingsFormatter = (tftRankings, tftBool, apexRanking, apexRankingBool) => {
     let str = '';
-    if (tftRankings) {
+    if (tftRankings && tftRankings.length > 0) {
         for (let i = 0; i < tftRankings.length; i++) {
             if (tftRankings[i].queueType === 'RANKED_TFT') {
                 str += `TFT Solo: ${tftRankings[i].tier} ${tftRankings[i].rank} - ${tftRankings[i].leaguePoints} LP\n`
@@ -9,7 +9,7 @@ const rankingsFormatter = (tftRankings, tftBool, apexRanking, apexRankingBool) =
                 str += `TFT Duos: ${tftRankings[i].tier} ${tftRankings[i].rank} - ${tftRankings[i].leaguePoints} LP\n`
             }
     } else {
-        if (tftBool) str += 'No TFT rankings found';
+        if (tftBool) str += `No TFT rankings found\n`;
     }
     if (apexRanking) {
         str += `Apex Ranked: ${apexRanking}\n`
