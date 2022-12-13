48343f8cda78876766fdaab4865f5430e545e949
bthomas2622 <bthomas2622@gmail.com>
2022-12-13 08:50:35 -0800

    use tft not lol api

--- bot-scripts/get-tft-ranking.js
+++ bot-scripts/get-tft-ranking.js
@@ -3,9 +3,9 @@ const axios = require("axios");
 const getTFTRankings = async (summonerName) => {
     let rankings = [];
     try {
-        const summonerData = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}?api_key=${process.env.RIOT_TFT_API_KEY}`);
+        const summonerData = await axios.get(`https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${summonerName}?api_key=${process.env.RIOT_TFT_API_KEY}`);
         const summonerEncryptedId = summonerData.data.id;
-        const summonerRankings = await axios.get(`https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerEncryptedId}?api_key=${process.env.RIOT_TFT_API_KEY}`);
+        const summonerRankings = await axios.get(`https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerEncryptedId}?api_key=${process.env.RIOT_TFT_API_KEY}`);
         if (!summonerRankings.data.length === 0) {
             for (const rank of summonerRankings.data) {
                 if (rank.queueType === "RANKED_TFT_DOUBLE_UP") {
