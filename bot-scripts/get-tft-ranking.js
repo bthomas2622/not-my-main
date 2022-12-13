01067a120f96db40dc168fc22e115353ad40d898
bthomas2622 <bthomas2622@gmail.com>
2022-12-13 08:55:50 -0800

    debugging tft

--- bot-scripts/get-tft-ranking.js
+++ bot-scripts/get-tft-ranking.js
@@ -6,7 +6,7 @@ const getTFTRankings = async (summonerName) => {
         const summonerData = await axios.get(`https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/${summonerName}?api_key=${process.env.RIOT_TFT_API_KEY}`);
         const summonerEncryptedId = summonerData.data.id;
         const summonerRankings = await axios.get(`https://na1.api.riotgames.com/tft/league/v1/entries/by-summoner/${summonerEncryptedId}?api_key=${process.env.RIOT_TFT_API_KEY}`);
-        if (!summonerRankings.data.length === 0) {
+        if (summonerRankings.data.length > 0) {
             for (const rank of summonerRankings.data) {
                 if (rank.queueType === "RANKED_TFT_DOUBLE_UP") {
                     rankings.push(rank);
