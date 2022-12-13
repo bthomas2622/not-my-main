0a7b5a8a8937f43e3f0cc8ef314bac3d7a8a28ec
bthomas2622 <bthomas2622@gmail.com>
2022-12-13 08:54:29 -0800

    debugging tft

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 
 const callApi = async () => {
-    const summonerData = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/freeBrunch?api_key=${process.env.RIOT_TFT_API_KEY}`);
+    const summonerData = await axios.get(`https://na1.api.riotgames.com/tft/summoner/v1/summoners/by-name/freeBrunch?api_key=${process.env.RIOT_TFT_API_KEY}`);
     console.log(summonerData);
 }
 
